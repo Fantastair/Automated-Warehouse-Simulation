2cd25f98f6b3ac9f5ca4014feb45f7f80ea5b1f2
Fantastair <2364209908@qq.com>
2025-05-23 02:15:07 +0800

    构建项目

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Automated-Warehouse-Simulation
-自动化仓储仿真 - 学以致用比赛作品
+>  自动化仓储仿真 - 学以致用比赛作品
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_mixer/SDL_mixer.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+int main(void)
+{
+    
+    
+    return 0;
+}
